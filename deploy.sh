8035cde777ea7adae9d5c41f0c15ca33f42118e0
kunsanglee <85242378+kunsanglee@users.noreply.github.com>
2023-08-09 01:55:17 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "> FE 배포"
-sudo cp -rf /home/ubuntu/sodev-project-front/build/* /home/ubuntu/sodev-project-front/build
+#sudo cp -rf /home/ubuntu/sodev-project-front/build/* /home/ubuntu/sodev-project-front/build
